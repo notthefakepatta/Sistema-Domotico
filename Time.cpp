ff0e7b9cdfd3383fdca3826096823c78d39cba5a
notthefakepatta <alessandro.pattarop@gmail.com>
2024-12-27 11:48:21 +0100

    Add files via upload

--- Time.cpp
+++ Time.cpp
@@ -4,6 +4,7 @@
 #include <iomanip>
 #include <stdexcept>
 
+/*  modifica di orario */
 void set_time(Time& t, int h, int m)
 {
     /*  controllo di input: non sono ovviamente consentiti
@@ -11,19 +12,12 @@ void set_time(Time& t, int h, int m)
     if (h<0 || h>23 || m<0 || m>59)
         throw std::invalid_argument("Invalid input");
 
-    /*  confronto tra il nuovo orario e quello che sta per essere
-     *  sostituito: non è consentito tornare indietro nel tempo */
-    Time t2;
-    t2.hours = h;
-    t2.minutes = m;
-
-    if (compareTo(const Time& t,const Time& t2)<0)
-        throw std::invalid_argument("Invalid input");
-
     /*  superati i controlli si effettua l'aggiornamento */
+    Time t2 = Time(h, m);
     t = t2;
 }
 
+/*  confronta due orari */
 int compareTo(const Time& t1, const Time& t2)
 {
     /*  distinzione dei casi. Il risultato fa riferimento al rapporto
@@ -38,7 +32,9 @@ int compareTo(const Time& t1, const Time& t2)
      *  hours_difference == 0. Negli altri due casi quest'ultimo non
      *  è necessario in quanto il risultato è già completamente definito */
     int hours_difference;
-    switch (hours_difference = t1.hours - t2.hours)
+    int h1 = t1.get_hours();
+    int h2 = t2.get_hours();
+    switch (hours_difference = h1 - h2)
     {
         case hours_difference > 0:
             return maggiore;
@@ -48,7 +44,9 @@ int compareTo(const Time& t1, const Time& t2)
         {
             /*  controllo della differenza tra i minuti */
             int minutes_difference;
-            switch (minutes_difference = t1.minutes - t2.minutes)
+            int m1 = t1.get_minutes();
+            int m2 = t2.get_minutes();
+            switch (minutes_difference = m1 - m2)
             {
                 case minutes_difference > 0:
                     return maggiore;
@@ -75,7 +73,7 @@ int compareTo(const Time& t1, const Time& t2)
  *  hh:mm, come da specifica */
 std::ostream& operator<<(std::ostream& os, const Time& t)
 {
-    os << std::setw(2) << std::setfill('0') << t.hours << ":";
-    os << std::setw(2) << std::setfill('0') << t.minutes;
+    os << std::setw(2) << std::setfill('0') << t.get_hours() << ":";
+    os << std::setw(2) << std::setfill('0') << t.get_minutes();
     return os;
 }
